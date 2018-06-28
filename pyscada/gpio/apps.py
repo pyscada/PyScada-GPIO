34ee1086b6e24317ab40e42bf140e98260777543
Martin Schroeder <m.schroeder@tu-berlin.de>
2018-06-28 11:18:52 +0200

    update to 0.7.0rc6
    0.7.0rc6
    - fixes of AppConfig for setup.py develop

--- pyscada/gpio/apps.py
+++ pyscada/gpio/apps.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import os
 from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
 
@@ -8,3 +9,4 @@ from django.utils.translation import ugettext_lazy as _
 class PyScadaGPIOConfig(AppConfig):
     name = 'pyscada.gpio'
     verbose_name = _("PyScada GPIO")
+    path = os.path.dirname(os.path.realpath(__file__))
