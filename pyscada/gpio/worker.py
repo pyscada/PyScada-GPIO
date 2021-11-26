7d8ab8d7a419e80b2a0940330c4d7f25483e36f8
clavay <clavayssiere@univ-pau.fr>
2021-11-26 15:28:12 +0100

    fix device with various subdevices

--- pyscada/gpio/worker.py
+++ pyscada/gpio/worker.py
@@ -4,6 +4,7 @@
 from __future__ import unicode_literals
 
 from pyscada.utils.scheduler import SingleDeviceDAQProcessWorker
+from pyscada.gpio import PROTOCOL_ID
 
 import logging
 
@@ -12,7 +13,7 @@ logger = logging.getLogger(__name__)
 
 
 class Process(SingleDeviceDAQProcessWorker):
-    device_filter = dict(gpiodevice__isnull=False)
+    device_filter = dict(gpiodevice__isnull=False, protocol_id=PROTOCOL_ID)
     bp_label = 'pyscada.gpio-%s'
 
     def __init__(self, dt=5, **kwargs):
